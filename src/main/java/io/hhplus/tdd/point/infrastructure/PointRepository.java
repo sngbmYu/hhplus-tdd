1687ad767352333a72772dbcdb8d3e420739be21
sngbmYu <dldkdlelqnzo@gmail.com>
2025-08-14 17:32:54 +0900

    test: PointService 포인트 조회 단위 테스트 작성 (Red)
    - 포인트 조회 관련 메서드 시그니처 추가
    - 포인트 조회 관련 단위 테스트 코드 추가

--- src/main/java/io/hhplus/tdd/point/infrastructure/PointRepository.java
+++ src/main/java/io/hhplus/tdd/point/infrastructure/PointRepository.java
@@ -1,4 +1,8 @@
 package io.hhplus.tdd.point.infrastructure;
 
+import io.hhplus.tdd.point.domain.UserPoint;
+
 public interface PointRepository {
+
+    UserPoint findById(long id);
 }
--- src/main/java/io/hhplus/tdd/point/infrastructure/PointRepositoryImpl.java
+++ src/main/java/io/hhplus/tdd/point/infrastructure/PointRepositoryImpl.java
@@ -1,7 +1,13 @@
 package io.hhplus.tdd.point.infrastructure;
 
+import io.hhplus.tdd.point.domain.UserPoint;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PointRepositoryImpl implements PointRepository {
+
+    @Override
+    public UserPoint findById(long id) {
+        return null;
+    }
 }
